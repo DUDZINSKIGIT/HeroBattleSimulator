e062823075375ed709c378f2018d2a355c8b08b6
DUDZINSKIGIT <mikdudzinski@wp.pl>
2023-07-08 22:32:34 +0200

    logo test

--- HeroBattleSimulator/HerobattleSimulator.py
+++ HeroBattleSimulator/HerobattleSimulator.py
@@ -4,3 +4,31 @@ key = 0
 h = 0
 m = 0
 e = 0
+dist = 0 
+regen = 0
+key = 0
+h = 0
+m=0
+e=0
+score=0
+attack=0
+response=0
+defence = 0
+atk=  0
+block = 0
+fight = 1
+win=  0
+loose = 0
+run = 0
+#poison mechanic
+bpoi = 0
+ebpoi = 0
+poi = 0
+epoi = 0
+def logo ():
+    f = open('logo.txt' , 'r')
+    file_contents = f.read()
+    print (file_contents)
+    f.close()
+
+    logo()
